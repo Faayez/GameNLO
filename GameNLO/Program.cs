4ee17e5b163e69ae701690f1a18703d64dbd6899
User <faayez@gmail.com>
2024-02-18 20:43:27 +0100

    Set default culture

--- GameNLO/Program.cs
+++ GameNLO/Program.cs
@@ -1,12 +1,18 @@
-
 using GameNLO.Services;
 using GameNLO.Services.GameEngines;
 using NLOGame.Hubs;
+using System.Globalization;
 namespace NLOGame;
+
 public class Program
 {
     public static void Main(string[] args)
     {
+        var cultureInfo = new CultureInfo("en-US");
+        cultureInfo.NumberFormat.CurrencySymbol = "€";
+        CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+        CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+
         var builder = WebApplication.CreateBuilder(args);
 
         builder.Services.AddRazorPages();
@@ -36,6 +42,7 @@ public class Program
         app.UseAuthorization();
 
         app.MapRazorPages();
+
         app.MapHub<StatusHub>("/StatusHub");
 
         app.Run();
